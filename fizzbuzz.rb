b6a08412ddd1f0e667258a7aa717d1181d6a1e57
Zarin Shahab <zarins23@gmail.com>
2018-02-14 00:36:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 && int % 5 == 0
-    puts "FizzBuzz"
+    "Fizz"
+  elsif int % 5 = 0
+    "Buzz"
+  elsif int % 3 && int % 5 = 0
+    "FizzBuzz"
   else
     nil
   end
